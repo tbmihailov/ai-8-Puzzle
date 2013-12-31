7f2cddbcb5aa5a9242b21a1484a64605b9db62f4
Todor B. Mihaylov <tbmihailov@gmail.com>
2013-12-31 02:21:41 +0200

    Priority function type

---
+++ AI.EightPuzzle/PriorityType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.EightPuzzle
+{
+    public enum PriorityType
+    {
+        Manhattan = 0,
+        Hamming = 1
+    }
+}
